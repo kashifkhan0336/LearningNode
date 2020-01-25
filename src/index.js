d9bc05322d7b63aabaea14856a1ea9eef59fb5ef
kashifkhan0336 <kashifkhan0336@gmail.com>
2020-01-25 11:25:27 +0500

    Selected new parser
    setted { useNewUrlParser: true }

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-
-
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("This is Home Page");
@@ -12,8 +10,10 @@ app.get("/posts", (req, res) => {
   res.send("We are on posts");
 });
 
-
-mongoose.connect("mongodb+srv://mastermind0336:XhvXyUR0TkFeBuwf@cluster0-j8qym.mongodb.net/test?retryWrites=true&w=majority", () =>
- console.log("Connected to DB!"))
+mongoose.connect(
+  "mongodb+srv://mastermind0336:XhvXyUR0TkFeBuwf@cluster0-j8qym.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => console.log("Connected to DB!")
+);
 
 app.listen(3000);
