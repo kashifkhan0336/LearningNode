f6db0f779b3b2f5f5c4300a5cd1180fffcdb19c6
kashifkhan0336 <kashifkhan0336@gmail.com>
2020-01-25 14:15:02 +0500

    Updated Folder Structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Import Routes
+const postsRoute = require("../routes/posts.js");
+app.use("/posts", postsRoute);
 
 app.get("/", (req, res) => {
   res.send("This is Home Page");
 });
 
-app.get("/posts", (req, res) => {
-  res.send("We are on posts");
-});
-
-mongoose.connect(
-  "mongodb+srv://mastermind0336:XhvXyUR0TkFeBuwf@cluster0-j8qym.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true },
-  () => console.log("Connected to DB!")
+//Database
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("Connected to DB!")
 );
 
 app.listen(3000);
