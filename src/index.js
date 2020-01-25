7cc1b26612148bfbe45792b820454549d40f416e
kashifkhan0336 <kashifkhan0336@gmail.com>
2020-01-25 11:21:28 +0500

    added Database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+
+
 
 app.get("/", (req, res) => {
   res.send("This is Home Page");
 });
+
+app.get("/posts", (req, res) => {
+  res.send("We are on posts");
+});
+
+
+mongoose.connect("mongodb+srv://mastermind0336:XhvXyUR0TkFeBuwf@cluster0-j8qym.mongodb.net/test?retryWrites=true&w=majority", () =>
+ console.log("Connected to DB!"))
 
 app.listen(3000);
